fe9ff42dd5f548b9c13008b85728c15572ffb0c4
SalimAbdala27 <salimabdala27@hotmail.com>
2022-06-11 23:53:52 +0100

    login working with display name and reset pass

--- sj-fitness/src/App.jsx
+++ sj-fitness/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState, useEffect } from 'react';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, sendPasswordResetEmail} from "firebase/auth"
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, sendPasswordResetEmail, updateProfile} from "firebase/auth"
 import { auth } from "./firebase-config"
 import './App.scss';
 
-// forgot password needs doing and adding a name to it too in the object
+// forgot password is done Needs a loading thing and a modal to say check spam etc... and adding a name to it too in the object done!!
+// above is all done because jerome is slacking smh :D
 
 function App() {
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
+  const [userName, setUserName] = useState("");
+  const [resetPassword, setResetPassword] = useState(false);
 
   const [user, setUser] = useState({});
 
@@ -23,11 +26,15 @@ function App() {
   const register = async () => {
     try {
       const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+      await updateProfile(user.user, {
+        displayName: userName
+      });
       console.log(user);
     } catch (error) {
       console.log(error.message);
     }
   }
+
 
   const login = async () => {
     try {
@@ -41,10 +48,20 @@ function App() {
   const logout = async () => {
     await signOut(auth);
   }
+
+  const resetPass = async () => {
+    try {
+      const user = await sendPasswordResetEmail(auth, loginEmail);
+      setResetPassword(true)
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
   <div className="App">
       <div>
         <h3>Register User</h3>
+        <input placeholder='name' onChange={(event) => setUserName(event.target.value)}/>
         <input placeholder='email' onChange={(event) => setRegisterEmail(event.target.value)}/>
         <input placeholder='password' onChange={(event) => setRegisterPassword(event.target.value)}/>
       </div>
@@ -55,8 +72,13 @@ function App() {
         <input placeholder='password' onChange={(event) => setLoginPassword(event.target.value)}/>
       </div>
       <button onClick={login}>Login</button>
+      <button onClick={resetPass}>Reset Pass</button>
+      {resetPassword && (
+        <p>Check Spam folder</p>
+      )}
       <h4>User Logged In:</h4>
-      {user?.email}
+      <p>{user?.displayName}</p>
+      <p>{user?.email}</p>
       <button onClick={logout}>Sign Out</button>
   </div>
   );
