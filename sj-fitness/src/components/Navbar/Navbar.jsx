20733b5e4c41b8d054cca81182b8b78a3a6fee2c
SalimAbdala27 <salimabdala27@hotmail.com>
2022-06-27 16:25:33 +0100

    register

--- sj-fitness/src/components/Navbar/Navbar.jsx
+++ sj-fitness/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./navbar.scss"
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineUser } from "react-icons/ai";
@@ -6,9 +6,23 @@ import { Link } from 'react-router-dom';
 // add icons to perfect it 
 const Navbar = (props) => {
   const {open, setOpen, user} = props;
+  const [firstLetter, setFirstLetter] = useState("")
+  const [userFirstLetter, setUserfirstLetter] = useState("")
   const openMenu = () => {
     setOpen((prevOpen) => !prevOpen);
   };
+  // const settingLetter = Array.from(user?.displayName)[0]
+  // useEffect(() => {
+  //   const check = () => {
+  //     setFirstLetter(user?.displayName)
+  //     console.log(firstLetter);
+  //     setUserfirstLetter(Array.from(firstLetter)[0])
+  //     console.log(userFirstLetter);
+  //   }
+  //   check()
+  // });
+  
+  // fix first letter thing it comes undefined || just put an inital
   return (
     <div className='navbar'>
       <div className="navbar__left">
@@ -17,7 +31,7 @@ const Navbar = (props) => {
       <div className="navbar__right">
         {user ?
             (<div className='usericon'>
-                <p className='usericon__letter'>{(user.displayName).charAt(0)}</p>
+                <p className='usericon__letter'>{user.displayName}</p>
             </div>)
             : <AiOutlineUser />
         }
--- sj-fitness/src/pages/Register/Register.jsx
+++ sj-fitness/src/pages/Register/Register.jsx
@@ -6,15 +6,17 @@ import "./register.scss"
 import {
   Link,
 } from "react-router-dom";
+import Button from '../../components/Button/Button';
 
 const Register = (props) => {
   const {user, setUser} = props
 // forgot password is done Needs a loading thing and a modal to say check spam and wrong pass response etc... and adding a name to it too in the object done!!
+// when logged in says welcome ... on the homepage
+// required fields!!! ask J about icons in here needs to be done with backgrouds
 const [registerEmail, setRegisterEmail] = useState("");
 const [registerPassword, setRegisterPassword] = useState("");
 const [userName, setUserName] = useState("");
 
-// const [user, setUser] = useState({});
 
 useEffect(() => {
   onAuthStateChanged(auth, (currentUser) => {
@@ -37,14 +39,19 @@ const register = async () => {
   
   return (
     <div className="register">
-      <div>
-        <h3>Register User</h3>
-        <input placeholder='name' onChange={(event) => setUserName(event.target.value)}/>
-        <input placeholder='email' onChange={(event) => setRegisterEmail(event.target.value)}/>
-        <input placeholder='password' onChange={(event) => setRegisterPassword(event.target.value)}/>
+      <div className='register__container'>
+        <h3 className='register__container-header'>Sign Up</h3>
+        <div className="register__container-form">
+          <p className="register__container-form-labels">Name</p>
+        <input className="register__container-form-input"onChange={(event) => setUserName(event.target.value)} required/>
+        <p className="register__container-form-labels">Email</p>
+        <input className="register__container-form-input" onChange={(event) => setRegisterEmail(event.target.value)} required/>
+        <p className="register__container-form-labels">Password</p>
+        <input className="register__container-form-input" onChange={(event) => setRegisterPassword(event.target.value)} required/>
+      <Link className='register__login' to={"/login"}><p className='register__login-p' >Already a user? <span className='register__login-span' >LOGIN</span></p></Link>
+      <button className='register__container-btn' onClick={register}>Sign Up</button>
+        </div>
       </div>
-      <button onClick={register}>Create user</button>
-      <Link to={"/login"}><button>Login</button></Link>
   </div>
   );
 }
