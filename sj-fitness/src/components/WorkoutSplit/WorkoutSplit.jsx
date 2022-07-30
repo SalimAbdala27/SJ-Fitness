51f858dde616299add14dbf25dabc2d45f561ffe
jeromekithinji <kithinjijerome@gmail.com>
2022-07-31 00:40:22 +0100

    Component styled, attempted add exercise func

--- sj-fitness/src/components/WorkoutSplit/WorkoutSplit.jsx
+++ sj-fitness/src/components/WorkoutSplit/WorkoutSplit.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./workoutSplit.scss";
-import { MdOutlineArrowDropDown, MdOutlineArrowDropUp } from "react-icons/md";
+import { MdOutlineArrowDropDown, MdOutlineArrowDropUp, MdPending, MdDoneOutline } from "react-icons/md";
 
 const WorkoutSplit = ({ opendropdown, splitName, workouts }) => {
     const mode = opendropdown ? (
@@ -19,21 +19,41 @@ const WorkoutSplit = ({ opendropdown, splitName, workouts }) => {
 
     workouts.map(exercise => {
         listOfWorkOuts.push(
-            <p className="options__links">{exercise}</p>
+            <p className="dropdown__options-exercise">{exercise} <MdPending className="dropdown__options-exercise-icon"/></p>
         )
     })
 
+    const inputRef = useRef(null);
+
+    const createExercise = () => {
+        console.log(inputRef.current.value);
+        listOfWorkOuts.push(
+            <p className="dropdown__options-exercise">{inputRef.current.value} <MdPending className="dropdown__options-exercise-icon"/></p>
+        )
+    }
+
     return (
-        <div className="dropdown">
-            <div className="dropdown__bar" onClick={handleChange}>
-                {splitName} {mode}
-            </div>
-            <div className="dropdown__options">
-                {openDrop && (
-                    <div className="options">
-                        {listOfWorkOuts}
+        <div className="pageHold">
+            <div className="dropdown">
+                <div className="dropdown__heading-container" onClick={handleChange}>
+                    <div className="dropdown__heading-title">
+                        {splitName}
                     </div>
-                )}
+                    <div className="dropdown__heading-icon">
+                        {mode}
+                    </div>
+                </div>
+                <div className="dropdown__options">
+                    {openDrop && (
+                        <div className="dropdown__options-list">
+                            {listOfWorkOuts}
+                            {/* <form onSubmit={createExercise}>
+                                <input className="dropdown__options-exercise-icon" placeholder="Add new exercise" ref={inputRef} />
+                                <button type="submit"> <MdDoneOutline /> </button>
+                            </form> */}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
