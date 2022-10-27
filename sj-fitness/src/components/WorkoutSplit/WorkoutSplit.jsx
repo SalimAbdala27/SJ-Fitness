9613e8e08491b1fc4651dbb42e505feaaf2a1f1b
jeromekithinji <kithinjijerome@gmail.com>
2022-10-27 14:02:26 +0100

    Moved workouts in array

--- sj-fitness/src/components/WorkoutSplit/WorkoutSplit.jsx
+++ sj-fitness/src/components/WorkoutSplit/WorkoutSplit.jsx
@@ -7,7 +7,10 @@ const WorkoutSplit = ({ opendropdown, splitName, workouts }) => {
 
     const [openDrop, setOpenDrop] = useState(false);
     const [openAdd, setOpenAdd] = useState(false);
+    const [arrayOfWorkouts, setArrayOfWorkouts] = useState([...workouts]);
     const [listOfWorkouts, setListOfWorkouts] = useState([]);
+    const [optionsPopup, setOptionsPopup] = useState(false)
+
 
     const mode = opendropdown ? (
         <MdOutlineArrowDropUp />
@@ -23,28 +26,65 @@ const WorkoutSplit = ({ opendropdown, splitName, workouts }) => {
         setOpenAdd((prevOpenDrop) => !prevOpenDrop);
     };
 
+    const handleOptionsPopup = () => {
+        console.log("Clicked");
+        setOptionsPopup((prevOpenDrop) => !prevOpenDrop);
+    };
+
     const initialiseWorkouts = () => {
         const workoutData = [];
-        workouts.map(exercise => {
+        console.log("Inital workouts ", arrayOfWorkouts);
+        arrayOfWorkouts.map(exercise => {
             workoutData.push(
-                <p className="dropdown__options-exercise">{exercise} <MdPending className="dropdown__options-exercise-icon"/></p>
+                <>
+                    <p className="dropdown__options-exercise" >
+                        {exercise} 
+                        <MdPending className="dropdown__options-exercise-icon" onClick={handleOptionsPopup} />
+                    </p>
+                    {/* {optionsPopup && ( */}
+                        <div className="options-popup">
+                            <li className="options-popup__item">View</li>
+                            <li className="options-popup__item">Edit</li>
+                            <li className="options-popup__item">Delete</li>
+                        </div>
+                    {/* )} */}
+                </>
             )
         })
         setListOfWorkouts(workoutData)
+        console.log("List of workouts after set: ",listOfWorkouts)
     }
 
     const inputRef = useRef(null);
 
     const createExercise = (e) => {
         e.preventDefault();
+        let updatedArrayWorkouts = [...arrayOfWorkouts];
+        updatedArrayWorkouts.push(inputRef.current.value);
         let updatedList = [...listOfWorkouts];
-        updatedList.push(<p className="dropdown__options-exercise">{inputRef.current.value} <MdPending className="dropdown__options-exercise-icon"/></p>);
+        updatedList.push(
+            <>
+                <p className="dropdown__options-exercise">
+                    {inputRef.current.value} 
+                    <MdPending className="dropdown__options-exercise-icon" onClick={handleOptionsPopup} />
+                </p>
+                {/* {optionsPopup && ( */}
+                    <div className="options-popup">
+                        <li className="options-popup__item">View</li>
+                        <li className="options-popup__item">Edit</li>
+                        <li className="options-popup__item">Delete</li>
+                    </div>
+                {/* )} */}
+            </>
+        );
         setListOfWorkouts(updatedList)
+        setArrayOfWorkouts(updatedArrayWorkouts)
+        console.log("After array ", arrayOfWorkouts);
     }
 
     useEffect(() => {
         initialiseWorkouts();
-    }, [])
+    }, [arrayOfWorkouts])
 
     return (
         <div className="pageHold">
